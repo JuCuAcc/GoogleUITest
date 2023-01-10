689e998538201f3618f92c79f28f746afa4b217f
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2023-01-10 20:26:38 +0600

    Complete

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 namespace GoogleUITest
 {
@@ -7,8 +9,37 @@ namespace GoogleUITest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SearchTextMatch()
         {
+            int waitingTime = 2000;
+
+            By googleSearchBar = By.Name("q");
+            By googleSearchButton = By.Name("btnK");
+            By googleResultText = By.XPath(".//div[@data-attrid='title']");
+            By googleIAgreeButton = By.Id("L2AGLb");
+
+            IWebDriver webDriver = new ChromeDriver();
+            Thread.Sleep(waitingTime);
+
+
+            webDriver.Navigate().GoToUrl("https://www.google.com/");
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleIAgreeButton).Click();
+            webDriver.Manage().Window.Maximize();
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchBar).SendKeys("Street Fighter V");
+            Thread.Sleep(waitingTime);
+
+            webDriver.FindElement(googleSearchButton).Click();
+            Thread.Sleep(waitingTime);
+
+            var actualResultText = webDriver.FindElement(googleResultText);
+
+            Assert.IsTrue(actualResultText.Text.Equals("Street Fighter V"));
+
+            webDriver.Quit();
         }
     }
 }
